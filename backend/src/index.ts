0c1eeb82645f956af15fb2731aba6d77128b8e62
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-11-21 16:32:39 -0300

    feat: :sparkles: add server with express and scripts updated
    The server was built with express and the 'dev', 'build' and 'start' scripts were updated and the 'test' script was removed for now."

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
