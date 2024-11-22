9dc7dd673bd7f6906e2604ee2a5f7926b8825352
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-11-22 20:15:28 -0300

    refactor: :recycle: updated EstimateRideRequestDTO to add custom validation for origin and destination fields

--- backend/src/dtos/EstimateRideRequestDTO.ts
+++ backend/src/dtos/EstimateRideRequestDTO.ts
@@ -1,9 +1,25 @@
 import { z } from 'zod';
 
-export const EstimateRideRequestDTO = z.object({
-  customer_id: z.string().min(1, { message: 'Customer ID is required' }),
-  origin: z.string().min(1, { message: 'Origin is required' }),
-  destination: z.string().min(1, { message: 'Destination is required' }),
-});
+export const EstimateRideRequestDTO = z
+  .object({
+    customer_id: z
+      .string()
+      .min(1, { message: 'O ID do usuário é obrigatório.' }),
+    origin: z
+      .string()
+      .min(1, { message: 'O endereço de Origem é obrigatório.' }),
+    destination: z
+      .string()
+      .min(1, { message: 'O endereço de Destino é obrigatório.' }),
+  })
+  .superRefine(({ origin, destination }, ctx) => {
+    if (destination === origin) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['destination'],
+        message: 'Os endereços de Origem e Destino não podem ser iguais.',
+      });
+    }
+  });
 
 export type EstimateRideRequestDTOType = z.infer<typeof EstimateRideRequestDTO>;
