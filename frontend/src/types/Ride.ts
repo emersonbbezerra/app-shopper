f22c916f433c95dc4f789e521d5c9610fb971819
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-11-26 19:12:50 -0300

    feat: :sparkles: add Driver and Ride interfaces

---
+++ frontend/src/types/Ride.ts
@@ -0,0 +1,13 @@
+export interface Ride {
+  id: string;
+  date: string;
+  origin: string;
+  destination: string;
+  distance: number;
+  duration: string;
+  driver: {
+    id: number;
+    name: string;
+  };
+  value: number;
+}
